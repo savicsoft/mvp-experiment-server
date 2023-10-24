3e044743bd5c013f53cd794e81dd573d0b902f41
sergienkoyura <sergienkoyura5@gmail.com>
2023-10-24 20:53:25 +0300

    Added User Entity

---
+++ src/main/java/com/savicsoft/carpooling/user/model/entity/User.java
@@ -0,0 +1,18 @@
+package com.savicsoft.carpooling.user.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="\"user\"")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+}
