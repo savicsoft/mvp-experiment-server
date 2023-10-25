baa96fccdc042590b679fd1215cea43b9ad1cf28
sergienkoyura <sergienkoyura5@gmail.com>
2023-10-25 12:36:28 +0300

    Changed the table name

--- src/main/java/com/savicsoft/carpooling/user/model/entity/User.java
+++ src/main/java/com/savicsoft/carpooling/user/model/entity/User.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name="\"user\"")
+@Table(name="carpool_user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
