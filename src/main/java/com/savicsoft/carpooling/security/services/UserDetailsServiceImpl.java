b36eb3ab083864430284813aa4f55d1caa720415
Luiz <luizardezzoni2301@gmail.com>
2023-10-26 15:02:28 -0300

    Minor changes and cleaning of the code

--- src/main/java/com/savicsoft/carpooling/security/services/UserDetailsServiceImpl.java
+++ src/main/java/com/savicsoft/carpooling/security/services/UserDetailsServiceImpl.java
@@ -21,9 +21,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Override
     @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        System.out.println(
-                "HERE22222222"
-        );
+
         User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
 
