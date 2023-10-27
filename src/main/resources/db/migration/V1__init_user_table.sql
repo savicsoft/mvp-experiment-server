d0024ab915ae5016c0eed3f0981d744e485e2079
Luiz <luizardezzoni2301@gmail.com>
2023-10-27 10:49:21 -0300

    Flyway bug fix

--- src/main/resources/db/migration/V1__init_user_table.sql
+++ src/main/resources/db/migration/V1__init_user_table.sql
@@ -1,7 +1,4 @@
 create table carpool_user(
-    id BIGSERIAL PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
-    password VARCHAR(120) NOT NULL,
-    email VARCHAR(50) NOT NULL
+    id BIGSERIAL PRIMARY KEY
 )
 
