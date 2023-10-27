7cbabf931375a999ee244afcfbfdd2df666be1ca
Ahmed Ibrahim <ahmedibrahim5182@gmail.com>
2023-10-27 15:44:18 +0200

    fixed SecurityConfig

--- src/main/java/com/savicsoft/carpooling/security/config/SecurityConfig.java
+++ src/main/java/com/savicsoft/carpooling/security/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.savicsoft.carpooling.security.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -10,7 +12,10 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig{
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
