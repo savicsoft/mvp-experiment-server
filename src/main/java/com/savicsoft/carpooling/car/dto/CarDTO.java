b1a92837488def7eb2ca6b3a31160ec4393ce25a
Ahmed Ibrahim <ahmedibrahim5182@gmail.com>
2023-11-01 04:52:50 +0200

    JWT updated

--- src/main/java/com/savicsoft/carpooling/car/dto/CarDTO.java
+++ src/main/java/com/savicsoft/carpooling/car/dto/CarDTO.java
@@ -1,25 +1,17 @@
 package com.savicsoft.carpooling.car.dto;
 
 import com.savicsoft.carpooling.car.enumeration.FuelType;
-import com.savicsoft.carpooling.car.model.entity.Car;
-import com.savicsoft.carpooling.car.service.CarService;
 import com.savicsoft.carpooling.user.model.entity.User;
-import jakarta.persistence.*;
-import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.experimental.SuperBuilder;
-import org.checkerframework.common.aliasing.qual.Unique;
-import org.springframework.beans.BeanUtils;
 
 import java.util.UUID;
 @Data
 @SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
-@RequiredArgsConstructor
 public class CarDTO {
 
     private UUID uuid;
