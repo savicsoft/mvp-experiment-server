e62a17f85b3980d77473c9a3733f7b866052ba59
Marko Gajic <gajic.marko@yahoo.com>
2023-11-16 23:03:16 +0100

    Added new method and adapter class to GCS

--- src/main/java/com/savicsoft/carpooling/googlecloudstorage/service/GoogleCloudStorageService.java
+++ src/main/java/com/savicsoft/carpooling/googlecloudstorage/service/GoogleCloudStorageService.java
@@ -5,7 +5,10 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.auth.oauth2.ServiceAccountCredentials;
 import com.google.cloud.storage.*;
 
-import lombok.RequiredArgsConstructor;
+import com.savicsoft.carpooling.exception.NotFoundException;
+import com.savicsoft.carpooling.exception.CouldNotCreateException;
+import com.savicsoft.carpooling.exception.CouldNotDeleteException;
+import com.savicsoft.carpooling.googlecloudstorage.adapter.MultipartFileAdapter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,8 +16,11 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Service
 public class GoogleCloudStorageService {
@@ -38,7 +44,7 @@ public class GoogleCloudStorageService {
         this.bucketName = bucketName;
     }
 
-    public Blob uploadFile(MultipartFile file, String fileName, UUID userUUID) throws IOException {
+    public Blob uploadFile(MultipartFile file, String fileName, UUID userUUID) {
         if (file == null || file.isEmpty() || fileName == null || fileName.isEmpty()) {
             throw new IllegalArgumentException("File and fileName cannot be null or empty.");
         }
@@ -51,16 +57,26 @@ public class GoogleCloudStorageService {
                 .setContentType(file.getContentType())
                 .setMetadata(metadata)
                 .build();
-
-        return storage.create(blobInfo, file.getBytes());
+        try {
+            Blob blob = storage.create(blobInfo, file.getBytes());
+            return blob;
+        } catch (StorageException | IOException e) {
+            throw new CouldNotCreateException("Internal Error. Could not upload file.");
+        }
     }
 
     public boolean deleteFile(String fileName) {
         if (fileName == null || fileName.isEmpty()) {
             throw new IllegalArgumentException("fileName cannot be null or empty.");
         }
-
-        return storage.delete(bucketName, fileName);
+        try {
+            Boolean deleted = storage.delete(bucketName, fileName);
+            if (!deleted)
+                throw new NotFoundException("File with specified name: " + fileName + "does not exist.");
+            return deleted;
+        } catch (StorageException e) {
+            throw new CouldNotDeleteException("Internal Error. Could not delete the file");
+        }
     }
 
     public Blob downloadFile(String fileName) {
@@ -69,5 +85,14 @@ public class GoogleCloudStorageService {
         }
 
         return storage.get(bucketName, fileName);
+    }
+
+    public List<MultipartFile> downloadFiles(String prefix) {
+        Iterable<Blob> blobs = storage.list(bucketName, Storage.BlobListOption.prefix(prefix)).iterateAll();
+        if (!blobs.iterator().hasNext())
+            throw new IllegalArgumentException("Specified path: " + prefix + "does not exist.");
+        return StreamSupport.stream(blobs.spliterator(), false)
+                .map(MultipartFileAdapter::new)
+                .collect(Collectors.toList());
     }
 }
