1b53fd0bb69f15b5b25826e8a561846a875cc0ad
Marko Gajic <gajic.marko@yahoo.com>
2023-11-18 13:07:03 +0100

    Fixed car repo and service

--- src/main/java/com/savicsoft/carpooling/car/repository/CarRepository.java
+++ src/main/java/com/savicsoft/carpooling/car/repository/CarRepository.java
@@ -4,19 +4,14 @@ import com.savicsoft.carpooling.car.model.entity.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface CarRepository extends JpaRepository<Car, Long> {
-
-    List<Car> findAllByUserId(Long id);
-    List<Car> findAll();
-    void deleteCarsByUserId(Long id);
-    void deleteById(Long car_id);
-    void deleteByRegistrationNumber(String registration);
-    Car findCarByRegistrationNumber(String registration);
-    Car findCarById(Long car_id);
-    Car findCarByUuid(UUID uuid);
+    Optional<List<Car>> findAllByUserId(UUID userId);
+    Optional<List<Car>> deleteCarsByUserId(UUID userId);
+    Optional<Car> deleteCarById(UUID carId);
+    Optional<Car> findCarById(UUID carId);
 }
--- src/main/java/com/savicsoft/carpooling/car/service/CarService.java
+++ src/main/java/com/savicsoft/carpooling/car/service/CarService.java
@@ -1,20 +1,17 @@
 package com.savicsoft.carpooling.car.service;
 
-import com.savicsoft.carpooling.car.dto.CarDTO;
+import com.savicsoft.carpooling.car.forms.UpdateCarInfoForm;
+import com.savicsoft.carpooling.car.model.dto.CarDTO;
 import com.savicsoft.carpooling.car.model.entity.Car;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface CarService {
-    List<CarDTO> getAllUsersCars(Long id);
-    List<CarDTO> getAllCars();
-    void deleteCarsByUserId(Long id);
-    void deleteById(Long id);
-    void deleteByRegistrationNumber(String registration);
-    CarDTO getCarByRegistrationNumber(String registration);
-    CarDTO getCarById(Long id);
+    List<CarDTO> getAllCarsOfUser(UUID uuid);
+    CarDTO getCarById(UUID uuid);
     CarDTO createCar(Car car);
-    CarDTO getCarByUUID(UUID uuid);
-
+    CarDTO updateCarInfo(UUID carId, UpdateCarInfoForm car);
+    List<CarDTO> deleteCarsByUserId(UUID id);
+    CarDTO deleteCarById(UUID carId);
 }
