e95246f2f85c05b1804074d6b63593eb674e3e5d
Marko Gajic <gajic.marko@yahoo.com>
2023-11-27 02:03:08 +0100

    Updated exception handler

--- src/main/java/com/savicsoft/carpooling/exception/MaxReachedException.java
+++ src/main/java/com/savicsoft/carpooling/exception/MaxReachedException.java
@@ -3,5 +3,5 @@ package com.savicsoft.carpooling.exception;
 import lombok.experimental.StandardException;
 
 @StandardException
-public class MaxCarsReachedException extends RuntimeException{
+public class MaxReachedException extends RuntimeException{
 }
--- src/main/java/com/savicsoft/carpooling/exception/handlers/GlobalExceptionHandler.java
+++ src/main/java/com/savicsoft/carpooling/exception/handlers/GlobalExceptionHandler.java
@@ -52,8 +52,8 @@ public class GlobalExceptionHandler {
             return new ResponseEntity<>(errorInfo, HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(MaxCarsReachedException.class)
-    public ResponseEntity<ErrorInfo> handleMaxCarsReachedException(Exception e) {
+    @ExceptionHandler(MaxReachedException.class)
+    public ResponseEntity<ErrorInfo> handleMaxReachedException(Exception e) {
 
         ErrorInfo errorInfo = new ErrorInfo();
         errorInfo.setMessage(e.getMessage());
