06d08122c0e9dbd2cb83dcb2f403d0979c6b473c
sergienkoyura <sergienkoyura5@gmail.com>
2024-01-22 09:26:17 +0200

    Secured Review endpoints

--- src/main/java/com/savicsoft/carpooling/review/service/impl/ReviewServiceImpl.java
+++ src/main/java/com/savicsoft/carpooling/review/service/impl/ReviewServiceImpl.java
@@ -1,9 +1,6 @@
 package com.savicsoft.carpooling.review.service.impl;
 
-import com.savicsoft.carpooling.exception.CouldNotCreateException;
-import com.savicsoft.carpooling.exception.CouldNotDeleteException;
-import com.savicsoft.carpooling.exception.CouldNotUpdateException;
-import com.savicsoft.carpooling.exception.NotFoundException;
+import com.savicsoft.carpooling.exception.*;
 import com.savicsoft.carpooling.review.model.dto.ReviewDTO;
 import com.savicsoft.carpooling.review.model.entity.Review;
 import com.savicsoft.carpooling.review.model.form.CreateReviewForm;
@@ -11,6 +8,8 @@ import com.savicsoft.carpooling.review.model.form.UpdateReviewForm;
 import com.savicsoft.carpooling.review.model.mapper.CustomReviewMapper;
 import com.savicsoft.carpooling.review.repository.ReviewRepository;
 import com.savicsoft.carpooling.review.service.ReviewService;
+import com.savicsoft.carpooling.security.auth.AuthUtil;
+import com.savicsoft.carpooling.user.model.entity.User;
 import com.savicsoft.carpooling.user.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -29,42 +28,49 @@ public class ReviewServiceImpl implements ReviewService {
     private final ReviewRepository reviewRepository;
     private final UserRepository userRepository;
     private final CustomReviewMapper customReviewMapper;
+    private final AuthUtil authUtil;
 
     @Override
     public List<ReviewDTO> findByRatedUserId(UUID ratedUserId) {
         //will rewrite to existsById from userService
-        if(!userRepository.existsById(ratedUserId)){
+        if (!userRepository.existsById(ratedUserId)) {
             throw new NotFoundException("User not found by id: " + ratedUserId);
         }
         return INSTANCE.mapToReviewDTOList(reviewRepository.findAllByRatedUser_Id(ratedUserId));
     }
 
     @Override
-    public List<ReviewDTO> findByRaterUserId(UUID raterUserId) {
-        if(!userRepository.existsById(raterUserId)){
-            throw new NotFoundException("User not found by id: " + raterUserId);
-        }
-        return INSTANCE.mapToReviewDTOList(reviewRepository.findAllByRaterUser_Id(raterUserId));
+    public List<ReviewDTO> findByRaterUser() {
+        String username = authUtil.getNameFromContext();
+        return INSTANCE.mapToReviewDTOList(reviewRepository.findAllByRaterUser_Email(username));
     }
 
     @Override
     public ReviewDTO findById(UUID id) {
-        return INSTANCE.mapToReviewDTO(reviewRepository.findById(id).orElseThrow(
-                () -> {
-                    throw new NotFoundException("Review not found by id: " + id);
-                }
-        ));
+        Review review = reviewRepository.findById(id).orElseThrow(() -> {
+            throw new NotFoundException("Review not found by id: " + id);
+        });
+        if (!review.getRaterUser().getUsername().equals(authUtil.getNameFromContext())) {
+            throw new ForbiddenException("You are not allowed to access this data!");
+        }
+
+        return INSTANCE.mapToReviewDTO(review);
     }
 
     @Override
     public ReviewDTO save(CreateReviewForm reviewForm) {
-        if(!userRepository.existsById(reviewForm.getRatedUserId())){
+        if (!userRepository.existsById(reviewForm.getRatedUserId())) {
             throw new NotFoundException("User not found by id: " + reviewForm.getRatedUserId());
         }
-        if(!userRepository.existsById(reviewForm.getRaterUserId())){
-            throw new NotFoundException("User not found by id: " + reviewForm.getRaterUserId());
-        }
+
+        String username = authUtil.getNameFromContext();
+        User user = userRepository.getUserByEmail(username).orElseThrow(() -> {
+            throw new NotFoundException("User not found by email: " + username);
+        });
+
         Review reviewToSave = customReviewMapper.mapToReview(reviewForm);
+        reviewToSave.setRaterUser(user);
+
         try {
             return INSTANCE.mapToReviewDTO(reviewRepository.save(reviewToSave));
         } catch (DataAccessException dae) {
@@ -77,6 +83,11 @@ public class ReviewServiceImpl implements ReviewService {
         Review review = reviewRepository.findById(id).orElseThrow(() -> {
             throw new NotFoundException("Review not found by id: " + id);
         });
+
+        if (!review.getRaterUser().getUsername().equals(authUtil.getNameFromContext())) {
+            throw new ForbiddenException("You are not allowed to access this data!");
+        }
+
         review.setRating(reviewToUpdate.getRating());
         review.setComment(reviewToUpdate.getComment());
         review.setAnonymous(reviewToUpdate.isAnonymous());
@@ -89,8 +100,19 @@ public class ReviewServiceImpl implements ReviewService {
 
     @Override
     public boolean delete(UUID id) {
-        if (reviewRepository.deleteById(id) != 1)
+        Review review = reviewRepository.findById(id).orElseThrow(() -> {
+            throw new NotFoundException("Review not found by id: " + id);
+        });
+
+        if (!review.getRaterUser().getUsername().equals(authUtil.getNameFromContext())) {
+            throw new ForbiddenException("You are not allowed to access this data!");
+        }
+
+        try {
+            reviewRepository.delete(review);
+        } catch (Exception e) {
             throw new CouldNotDeleteException("Internal Error. Could not delete a review.");
+        }
         return true;
     }
 
